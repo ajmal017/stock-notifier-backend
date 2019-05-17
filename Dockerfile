93a36a8f8e7a7a1b9a498f19cc478ad0d62e16dc
Alden Perrine <aldenperrine@gmail.com>
2019-05-17 12:05:34 -0700

    Making server only login one user and update DB

--- Dockerfile
+++ Dockerfile
@@ -25,14 +25,12 @@ RUN mkdir /clibs
 # GMP
 ADD gmp-6.1.2.tar.xz /clibs/
 RUN cd /clibs/gmp-6.1.2/ &&  ./configure --prefix=$HOME/.local && \
-    make && make install
-RUN rm -rf /clibs/gmp-6.1.2
+    make && make install && rm -rf /clibs/gmp-6.1.2
 
 # libSodium
 ADD libsodium-1.0.16.tar.gz /clibs/
 RUN cd /clibs/libsodium-1.0.16/ &&  ./configure --prefix=$HOME/.local && \
-    make && make install
-RUN rm -rf /clibs/libsodium-1.0.16
+    make && make install && rm -rf /clibs/libsodium-1.0.16
 
 ENV LD_LIBRARY_PATH "/root/.local/lib:$LD_LIBRARY_PATH"
 
@@ -57,8 +55,7 @@ RUN apt-get install -y google-chrome-stable
 # install chromedriver
 RUN apt-get install -yqq unzip
 RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
-RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
-RUN rm /tmp/chromedriver.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/ && rm /tmp/chromedriver.zip
 
 RUN pip install selenium
 
--- server.py
+++ server.py
@@ -12,6 +12,10 @@ def run_server():
     stock_data = scraper.getStockData(symbols)
     print(stock_data)
 
+    for symbol, data in stock_data.items():
+        database.updateSupports(symbol, [i[0] for i in data['Supports']])
+        database.updateResistances(symbol, [i[0] for i in data['Resistances']])
+        
     user_tickers = database.getTickersFromUser('Aldaddy')
     print(user_tickers)
 
@@ -24,20 +28,19 @@ def run_server():
     server_address = ('0.0.0.0', 8000)
     sock.bind(server_address)
     sock.listen(1)
-    while True:
-        connection, client_address = sock.accept()
-        s_bytes = connection.recv(128)
-        v_bytes = connection.recv(128)
-        A_bytes = connection.recv(128)
-        b_bytes, B_bytes = server_lib.generate_b(v_bytes)
-        connection.send(B_bytes)
-        m1_bytes, m2_bytes = server_lib.generate_ss(A_bytes, b_bytes, B_bytes, v_bytes)
-        mv = connection.recv(64)
-        connection.send(m2_bytes)
-        if mv == m1_bytes:
-            print("Login success :)")
-        else:
-            print("Login failure :(")
+    connection, client_address = sock.accept()
+    s_bytes = connection.recv(128)
+    v_bytes = connection.recv(128)
+    A_bytes = connection.recv(128)
+    b_bytes, B_bytes = server_lib.generate_b(v_bytes)
+    connection.send(B_bytes)
+    m1_bytes, m2_bytes = server_lib.generate_ss(A_bytes, b_bytes, B_bytes, v_bytes)
+    mv = connection.recv(64)
+    connection.send(m2_bytes)
+    if mv == m1_bytes:
+        print("Login success :)")
+    else:
+        print("Login failure :(")
 
 if __name__ == "__main__":
     run_server()

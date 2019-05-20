58d551aa34978b0e284df56ca256ac2a2957757e
Spenser Wong <spenserwong1@gmail.com>
2019-05-20 12:47:46 -0700

    Change API to allow module to send notifications to multiple tokens

--- notifications.py
+++ notifications.py
@@ -1,17 +1,19 @@
 import requests
 import json
 
-def main():
-	endpoint = "https://fcm.googleapis.com/fcm/send"
-	headers = {"Content-Type": "application/json",
-	"Authorization": "key="}
-	data = {"notification" : {
-	 "body" : "This is a test",
-	 "title": "Test"
-	 }
-	}
-	r = requests.post(url = endpoint, data=json.dumps(data), headers=headers)
+def sendNotification(tokens, title, body):
+	for token in tokens:
+		endpoint = "https://fcm.googleapis.com/fcm/send"
+		headers = {"Content-Type": "application/json",
+		"Authorization": "key="} #Add firebase authentication key 
+		data = {"notification" : {
+		 "body" : body,
+		 "title": title
+		 },
+		 "to": token
+		}
+		r = requests.post(url = endpoint, data=json.dumps(data), headers=headers)
 
 
-if __name__== "__main__":
-  main()
+#if __name__== "__main__":
+#  main()
